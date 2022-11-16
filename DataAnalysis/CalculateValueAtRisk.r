93ab3b34d0d93d3e7d3c0afbcfb60daf0cd88b62
Oshimani <jannick2.0@googlemail.com>
2022-11-16 13:16:10 +0100

    fixed missing referece to confidence_level

--- DataAnalysis/CalculateValueAtRisk.r
+++ DataAnalysis/CalculateValueAtRisk.r
@@ -18,17 +18,17 @@ mercedes_benz_group_data <- get_prices(start_date, end_date, MERCEDES_BENZ_GROUP
 deutsche_bank_value_at_risk <- get_value_at_risk(deutsche_bank_data, "dailyreturns", alpha)
 deutsche_bank_value_at_risk
 
-mercedes_benz_group_value_at_risk <- get_value_at_risk(mercedes_benz_group_data, "dailyreturns", confidence_level)
+mercedes_benz_group_value_at_risk <- get_value_at_risk(mercedes_benz_group_data, "dailyreturns", alpha)
 mercedes_benz_group_value_at_risk
 
 deutsche_bank_value_at_risk <- get_statistisch_daily_returns(deutsche_bank_data, "dailyreturns")
 deutsche_bank_value_at_risk
 
-deutsche_bank_value_at_risk_statistisch <- get_value_at_risk_statistisch(deutsche_bank_data, "dailyreturns", confidence_level)
+deutsche_bank_value_at_risk_statistisch <- get_value_at_risk_statistisch(deutsche_bank_data, "dailyreturns", alpha)
 deutsche_bank_value_at_risk_statistisch
 
 mercedes_benz_group_value_at_risk <- get_statistisch_daily_returns(mercedes_benz_group_data, "dailyreturns")
 mercedes_benz_group_value_at_risk
 
-mercedes_benz_group_value_at_risk_statistisch <- get_value_at_risk_statistisch(mercedes_benz_group_data, "dailyreturns", confidence_level)
+mercedes_benz_group_value_at_risk_statistisch <- get_value_at_risk_statistisch(mercedes_benz_group_data, "dailyreturns", alpha)
 mercedes_benz_group_value_at_risk_statistisch
