e424b3d546faae7d285c77210fa9b16fd6d7469c
Oshimani <jannick2.0@googlemail.com>
2023-01-18 13:15:45 +0100

    png logo

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -52,7 +52,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
             <Stack spacing={4} mb={8} direction="row" justifyContent={"center"}>
               <Link title="Back to Home" style={{ color: "inherit", textDecoration: "none" }} href="/">
                 <Stack spacing={1} direction="column" alignItems="center">
-                  <Image src={"/watchItLogo.svg"} alt="W@tch IT" height={114} width={401} />
+                  <Image src={"/logo.png"} alt="W@tch IT" height={114} width={401} />
                 </Stack>
               </Link>
             </Stack>
